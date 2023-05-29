4fe289c7935997345da7c69f633baedbeaa6853d
linehad <91234912+linehad@users.noreply.github.com>
2023-05-29 23:51:42 +0900

    Add files via upload

---
+++ Othello/OthelloPlayerController.cpp
@@ -0,0 +1,98 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OthelloPlayerController.h"
+#include "GameInfoStruct.h"
+#include "ServerGameStateBase.h"
+#include "GameBoard.h"
+#include "OthelloPices_UserWidget.h"
+#include "OthelloGameModeBase.h"
+
+#include <Blueprint/UserWidget.h>
+#include <Components/Image.h>
+
+void AOthelloPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	bShowMouseCursor = true;
+	// SetInputMode에 FInputModeGameAndUI를 넣어두면 게임과 UI모두 입력이 가능한 상태가 된다.
+	SetInputMode(FInputModeGameAndUI());
+
+	GameMode = GetWorld()->GetAuthGameMode<AOthelloGameModeBase>();
+}
+
+void AOthelloPlayerController::OthelloNextturn_Implementation(int ButtonIndex)
+{
+	AServerGameStateBase* OthelloState = Cast<AServerGameStateBase>(GetWorld()->GetGameState());
+	UGameBoard* OthelloBoard = Cast<UGameBoard>(CurrentWidget);
+	OthelloBoard->SetSeconds();
+	//OthelloBoard->SetTime(OthelloState->GetLimitTime());
+
+	GameMode->OthelloNextTurn(ButtonIndex);
+}
+
+void AOthelloPlayerController::PutPieces_Implementation(int index)
+{
+}
+
+void AOthelloPlayerController::ResetBoardWidget_Implementation(TSubclassOf<UUserWidget> NewWidgetClass, int32 size, int32 time)
+{
+	if (CurrentWidget != nullptr)
+	{
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+
+	if (BoardWidget != nullptr)
+	{
+		BoardWidget->RemoveFromViewport();
+		BoardWidget = nullptr;
+	}
+
+	if (NewWidgetClass != nullptr)
+	{
+		BoardWidget = CreateWidget(GetWorld(), NewWidgetClass);
+		CurrentWidget = BoardWidget;
+
+		if (BoardWidget != nullptr)
+		{
+			BoardWidget->AddToViewport();
+			Cast<UGameBoard>(BoardWidget)->StartSet();
+		}
+	}
+}
+
+void AOthelloPlayerController::ChangeWidget_Implementation(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+	if (CurrentWidget != nullptr)
+	{
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+
+	if (NewWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget(GetWorld(), NewWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+
+	if (CurrentWidget != nullptr && CurrentWidget == Cast<UGameBoard>(CurrentWidget))
+	{
+		Cast<UGameBoard>(CurrentWidget)->StartTimer();
+	}
+}
+
+void AOthelloPlayerController::SetBoardCoordinate_Implementation(int x, int y)
+{
+	//othelloArrIndex = (Cast<AServerGameStateBase>(GetWorld()->GetGameState())->GetBoardSize() * y) + x;
+	GameMode->OthelloChangeTurn(x, y);
+}
+
+void AOthelloPlayerController::Server_ReverseTurn_Implementation()
+{
+	GameMode->ReverseTurn();
+}
